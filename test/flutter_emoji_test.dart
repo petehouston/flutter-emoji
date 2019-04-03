032ae31863cb95ed72cdc09a94b033840bc9329c
Pete Houston <contact@petehouston.com>
2019-04-03 14:29:37 +0700

    Update documentation and test.

--- test/flutter_emoji_test.dart
+++ test/flutter_emoji_test.dart
@@ -3,55 +3,58 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_emoji/flutter_emoji.dart';
 
 void main() {
-  var emoji = Emoji();
+  var emojiParser = EmojiParser();
+  var emojiCoffee = Emoji('coffee', '☕');
+  var emojiHeart = Emoji('heart', '❤️');
 
   test('get', () {
-    expect(emoji.get('coffee'), '☕');
-    expect(emoji.get(':coffee:'), '☕');
+    expect(emojiParser.get('coffee'), emojiCoffee);
+    expect(emojiParser.get(':coffee:'), emojiCoffee);
 
-    expect(emoji.get('does_not_exist'), null);
-    expect(emoji.get(':does_not_exist:'), null);
+    expect(emojiParser.get('does_not_exist'), Emoji.None);
+    expect(emojiParser.get(':does_not_exist:'), Emoji.None);
   });
 
   test('emoji name', () {
-    expect(emoji.hasName('coffee'), true);
-    expect(emoji.getName('coffee'), '☕');
+    expect(emojiParser.hasName('coffee'), true);
+    expect(emojiParser.getName('coffee'), emojiCoffee);
 
-    expect(emoji.hasName('does_not_exist'), false);
-    expect(emoji.getName(':does_not_exist:'), null);
+    expect(emojiParser.hasName('does_not_exist'), false);
+    expect(emojiParser.getName(':does_not_exist:'), Emoji.None);
   });
 
   test('emoji info', () {
-    var heart = emoji.info('heart');
-    expect(heart != null, true);
+    var heart = emojiParser.info('heart');
 
-    expect(heart is Map<String, dynamic>, true);
-    expect(heart['name'], 'heart');
-    expect(heart['full'], ':heart:');
-    expect(heart['emoji'], '❤️');
+    expect(heart != null, true);
+    expect(heart is Emoji, true);
+
+    expect(heart.name, 'heart');
+    expect(heart.full, ':heart:');
+    expect(heart.code, '❤️');
   });
 
   test('emoji code', () {
-    expect(emoji.hasEmoji('❤️'), true);
-    expect(emoji.getEmoji('❤️'), 'heart');
+    expect(emojiParser.hasEmoji('❤️'), true);
+    expect(emojiParser.getEmoji('❤️'), emojiHeart);
 
-    expect(emoji.hasEmoji('p'), false);
-    expect(emoji.getEmoji('p'), null);
+    expect(emojiParser.hasEmoji('p'), false);
+    expect(emojiParser.getEmoji('p'), Emoji.None);
   });
 
   test('emojify a text', () {
-    expect(emoji.emojify('I :heart: :coffee:'), 'I ❤️ ☕');
+    expect(emojiParser.emojify('I :heart: :coffee:'), 'I ❤️ ☕');
 
-    expect(emoji.emojify('I :love coffee:'), 'I :love coffee:');
-    expect(emoji.emojify('I :love :coffee'), 'I :love :coffee');
-    expect(emoji.emojify('I love: :coffee'), 'I love: :coffee');
-    expect(emoji.emojify('I love: coffee:'), 'I love: coffee:');
+    expect(emojiParser.emojify('I :love coffee:'), 'I :love coffee:');
+    expect(emojiParser.emojify('I :love :coffee'), 'I :love :coffee');
+    expect(emojiParser.emojify('I love: :coffee'), 'I love: :coffee');
+    expect(emojiParser.emojify('I love: coffee:'), 'I love: coffee:');
   });
 
   test('unemojify a text', () {
-    expect(emoji.unemojify('I ❤️ car'), 'I :heart: car');
+    expect(emojiParser.unemojify('I ❤️ car'), 'I :heart: car');
 
-    expect(emoji.unemojify('I heart car'), 'I heart car');
-    expect(emoji.unemojify('I :heart: car'), 'I :heart: car');
+    expect(emojiParser.unemojify('I heart car'), 'I heart car');
+    expect(emojiParser.unemojify('I :heart: car'), 'I :heart: car');
   });
 }
