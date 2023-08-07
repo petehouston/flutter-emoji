7443bd4271bf677a4a61da1384574a810714124d
Pete Houston <contact@petehouston.com>
2023-08-07 00:49:09 -0500

    Remove equalList helper func

--- test/flutter_emoji_test.dart
+++ test/flutter_emoji_test.dart
@@ -169,19 +169,19 @@ void main() {
   });
 
   test('parse Emojis', () {
-    var equalList = (var list1, var list2) {
-      if (!(list1 is List && list2 is List) || list1.length != list2.length) {
-        return false;
-      }
-
-      for (int i = 0; i < list1.length; i++) {
-        if (list1[i] != list2[i]) {
-          return false;
-        }
-      }
-
-      return true;
-    };
+    // var equalList = (var list1, var list2) {
+    //   if (!(list1 is List && list2 is List) || list1.length != list2.length) {
+    //     return false;
+    //   }
+    //
+    //   for (int i = 0; i < list1.length; i++) {
+    //     if (list1[i] != list2[i]) {
+    //       return false;
+    //     }
+    //   }
+    //
+    //   return true;
+    // };
 
     expect(emojiParser.parseEmojis(''), []);
     expect(emojiParser.parseEmojis('I ❤️ Flutter just like ☕'), ['❤️', '☕']);
